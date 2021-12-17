af64670273881d20898ff5d37fb6b64c92fd908d
HYUNJEONGKIM <sally.hyunjeong.kim@gmail.com>
2021-12-17 17:00:47 +0800

    Initial configuration for react, typescript, eslint and prettier with snowpack

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+interface AppProps {}
+
+function App({}: AppProps) {
+    return <div className="App">Learn React</div>;
+}
+
+export default App;
