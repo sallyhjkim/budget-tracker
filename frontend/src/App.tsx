3ae2c412efeade5f4dba254da42bb621a1ad37d4
HYUNJEONGKIM <sally.hyunjeong.kim@gmail.com>
2021-12-17 17:51:40 +0800

    Add antd ui library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "antd";
 
 interface AppProps {}
 
 function App({}: AppProps) {
-    return <div className="App">Learn React</div>;
+    return (
+        <div className="App">
+            <Button
+                type="primary"
+                onClick={() => {
+                    console.log(11);
+                }}
+            >
+                Button
+            </Button>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
-  import.meta.hot.accept();
+    import.meta.hot.accept();
 }
